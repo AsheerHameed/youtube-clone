1e66cd8a9f32bf635e0c9e9b59ada0a4a50fd0d3
asheerhameed <asheerhameed54321@gmail.com>
2023-03-25 17:37:43 +0530

    build several components and set up rapid api

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Videos, Sidebar } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchFromAPI(`/search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items))
+  }, [selectedCategory]);
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          varaint="body2"
+          sx={{ mt: "1.5", color: "#fff" }}
+        >
+          Copyright AsheerHameed
+        </Typography>
+      </Box>
 
-export default Feed
+      <Box p={2} sx={{ overFlowY: "auto", flex: 2, height: "90vh" }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#df2517" }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
